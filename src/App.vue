1f590234acbd4da550f403983df0012323ebfe72
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-10-20 22:19:09 +0600

    practiced vue reactive and ref utilities

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Reactivity from "./components/Reactivity.vue";
 import Ref from "./components/Ref.vue";
+import ReactiveUtilities from "./components/ReactiveUtilities.vue";
 </script>
 
 <template>
   <h1>App component</h1>
   <hr />
-  <Ref></Ref>
+  <ReactiveUtilities></ReactiveUtilities>
+  <!-- <Ref></Ref> -->
   <!-- <Reactivity></Reactivity> -->
 </template>
 
---
+++ src/components/ReactiveUtilities.vue
@@ -0,0 +1,40 @@
+<template>
+  <h2>reactive and ref utilities</h2>
+</template>
+
+<script setup>
+import { reactive, isReactive, isProxy, isRef, ref, unref } from "vue";
+
+let a = 1;
+let b = ref(2);
+let c = reactive([0, 1, 2, 3]);
+let d = reactive({ name: "sakil", age: 34 });
+
+console.log("a is = ", a);
+console.log("b is = ", b);
+console.log("c is = ", c);
+
+console.log("a is ref = ", isRef(a));
+console.log("b is ref = ", isRef(b));
+console.log("c is ref = ", isRef(c));
+
+console.log("a is reactive = ", isReactive(a));
+console.log("b is reactive = ", isReactive(b));
+console.log("c is reactive = ", isReactive(c));
+
+console.log("a is proxy = ", isProxy(a));
+console.log("b is proxy = ", isProxy(b));
+console.log("c is proxy = ", isProxy(c));
+
+console.log("b after unref = ", unref(b));
+
+console.log("d is proxy = ", isProxy(d));
+console.log("d is reactive = ", isReactive(d));
+console.log("d is ref = ", isRef(d));
+
+console.log("d.name is reactive = ", isReactive(d.name));
+console.log("d.name is proxy = ", isProxy(d.name));
+console.log("d.name is ref = ", isRef(d.name));
+</script>
+
+<style></style>
