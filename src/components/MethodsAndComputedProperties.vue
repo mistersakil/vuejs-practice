579a6f05d29d03a6edcd0d3660c20109f16c0c6c
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-10-21 09:08:14 +0600

    vue method implementation

--- src/components/MethodsAndComputedProperties.vue
+++ src/components/MethodsAndComputedProperties.vue
@@ -1,9 +1,22 @@
 <template>
   <h2>method and computed property</h2>
+  <p>full name = {{ fullName() }}</p>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { reactive, ref } from "vue";
+let firstName = ref("sakil");
+let lastName = ref("mahmud");
+const fullName = () => {
+  return `${firstName.value} ${lastName.value}`;
+};
+
+setTimeout(() => {
+  firstName.value = "rasel";
+  lastName.value = "jomadder";
+  console.log(firstName);
+  console.log(lastName);
+}, 2000);
 </script>
 
 <style></style>
