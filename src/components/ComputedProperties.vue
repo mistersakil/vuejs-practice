8d3429fafdfeacd75f0e05f02c8f282859728c5b
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-10-21 09:47:13 +0600

    implemented computed property setter and getter

--- src/components/ComputedProperties.vue
+++ src/components/ComputedProperties.vue
@@ -2,9 +2,7 @@
   <h2>computed properties</h2>
 
   <p>fullName = {{ fullName }}</p>
-  <p>fullName = {{ fullName }}</p>
-  <p>fullName = {{ fullName }}</p>
-  <p>fullName = {{ fullName }}</p>
+  <p>fullName2 = {{ fullName2 }}</p>
 </template>
 
 <script setup>
@@ -14,13 +12,27 @@ let firstName = ref("sakil");
 let lastName = ref("mahmud");
 
 const fullName = computed(() => {
+  console.log("computed called");
   return `${firstName.value} ${lastName.value}`;
 });
 
 setTimeout(() => {
+  console.log("timeout called");
   firstName.value = "rasel";
   lastName.value = "jomadder";
-}, 2000);
+}, 5000);
+
+const fullName2 = computed({
+  get() {
+    console.log("getter called");
+    return `${firstName.value} ${lastName.value}`;
+  },
+
+  set(newValue) {
+    console.log("setter called");
+    return ([firstName.value, lastName.value] = newValue.split(" "));
+  },
+});
 </script>
 
 <style></style>
