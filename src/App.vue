25b974f586d3143f95619f02da39422db1002a09
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-04-21 09:35:36 +0600

    learning vue component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,8 @@
 <script setup>
-// import Reactivity from "./components/Reactivity.vue";
-// import Ref from "./components/Ref.vue";
-// import ReactiveUtilities from "./components/ReactiveUtilities.vue";
-// import Methods from "./components/Methods.vue";
-// import ComputedProperties from "./components/ComputedProperties.vue";
-// import ConditionalRendering from "./components/ConditionalRendering.vue";
-// import ListRendering from "./components/ListRendering.vue";
-// import Event from "./components/Event.vue";
-// import Form from "./components/Form.vue";
-import VueProps from "./components/VueProps.vue";
 </script>
 
 <template>
   <h1>App component</h1>
-  <hr />
-  <VueProps fName="sakil" lName="mahmud" />
-  <!-- <Form /> -->
-  <!-- <Event /> -->
-  <!-- <ListRendering /> -->
-  <!-- <ConditionalRendering /> -->
-  <!-- <ComputedProperties></ComputedProperties> -->
-  <!-- <Methods></Methods> -->
-  <!-- <ReactiveUtilities></ReactiveUtilities> -->
-  <!-- <Ref></Ref> -->
-  <!-- <Reactivity></Reactivity> -->
 </template>
 
 <style scoped></style>
