1893c2e968fe7504fded7eb58f9eda56efa98763
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-04-27 23:34:05 +0600

    src/components2/css.vue css component created

--- src/Components2/Methods2.vue
+++ src/Components2/Methods2.vue
@@ -1,21 +1,7 @@
 <script setup>
-import { ref, isRef } from 'vue'
-let person = {
-    name: ref('sakil'),
-    age: ref(25)
-}
+import { ref } from 'vue';
+let $name = ref("sakil");
 
-function getUserDetails() {
-    const { name, age } = person;
-    return `name is ${name.value} and age is ${age.value} `
-}
-
-setTimeout(() => {
-    console.log('timeout called');
-    person.name.value = 'rasel'
-    person.age.value = '30'
-    console.log(person);
-}, 2000);
 </script>
 
 <template>
