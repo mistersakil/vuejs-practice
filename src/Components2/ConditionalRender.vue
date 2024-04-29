faaf5d6e700518cdf763cefbd30444568d2350be
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-04-29 09:51:09 +0600

    vue v-if v-else-if v-else practive

--- src/Components2/ConditionalRender.vue
+++ src/Components2/ConditionalRender.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue'
 let show = ref(true)
 let show2 = ref(false)
 
+const marks = ref(34);
 </script>
 
 
@@ -13,4 +14,16 @@ let show2 = ref(false)
 
     <!-- this will not show  -->
     <button v-if="show2">this btn will not show</button>
+
+    <!-- show result grade based on marks using v-if v-else-if v-else  -->
+    <h4>Your grade is: </h4>
+    <h2 v-if="marks > 100">invalid marks</h2>
+    <h2 v-else-if="marks >= 80 && marks <= 100">A+</h2>
+    <h2 v-else-if="marks >= 70 && marks < 80">A</h2>
+    <h2 v-else-if="marks >= 60 && marks < 70">A-</h2>
+    <h2 v-else-if="marks >= 50 && marks < 60">B</h2>
+    <h2 v-else-if="marks >= 40 && marks < 50">C</h2>
+    <h2 v-else>Take preparation again</h2>
+
+
 </template>
