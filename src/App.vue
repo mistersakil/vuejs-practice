948addfaf11456c34d16ca81530d19de8c676bee
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-04-29 09:40:32 +0600

    vue v-if learning

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,13 @@
 // import AttrBinding from './Components2/AttrBinding.vue';
 // import ToRefs from './Components2/ToRefs.vue';
 // import Methods2 from './Components2/Methods2.vue'
-
-import Css from './Components2/Css.vue';
-
+// import Css from './Components2/Css.vue';
+import ConditionalRender from './Components2/ConditionalRender.vue';
 
 </script>
 
 <template>
-  <Css></Css>
+  <ConditionalRender>
+
+  </ConditionalRender>
 </template>
-
-
---
+++ src/Components2/ConditionalRender.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+let show = ref(true)
+let show2 = ref(false)
+
+</script>
+
+
+<template>
+    <h1>conditional rendering</h1>
+    <button v-if="show">this is a visible btn</button>
+
+    <!-- this will not show  -->
+    <button v-if="show2">this btn will not show</button>
+</template>
