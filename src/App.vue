5ad811710170f64843679573ffe92da4868de01b
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-07-01 21:00:12 +0600

    working on vue event

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
 <script setup>
-// import Hello from './Components2/Hello.vue';
-// import AttrBinding from './Components2/AttrBinding.vue';
-// import ToRefs from './Components2/ToRefs.vue';
-// import Methods2 from './Components2/Methods2.vue'
-// import Css from './Components2/Css.vue';
-import ConditionalRender from './Components2/ConditionalRender.vue';
-
+import Event from './Components3/Event.vue';
 </script>
 
 <template>
-  <ConditionalRender>
-
-  </ConditionalRender>
+  <Event />
 </template>
---
+++ src/Components3/Event.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+let count = ref(0)
+function increment() {
+    count.value++;
+}
+function decrement() {
+    count.value--;
+}
+</script>
+
+<template>
+    <button @click="increment">+</button> -
+    <button @click="decrement">-</button>
+    <div>count: {{ count }}</div>
+</template>
+
+<style></style>
