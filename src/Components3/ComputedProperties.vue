7abdd469c20926af98e5db77908aef02bd024608
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-07-02 23:55:29 +0600

    computed propery on increment and decrement

--- src/Components3/ComputedProperties.vue
+++ src/Components3/ComputedProperties.vue
@@ -5,9 +5,16 @@ let number = ref(2);
 let squNum = computed(function () {
     return number.value * number.value;
 });
+
+function decrement() {
+    number.value--;
+}
 </script>
 
 <template>
     <h1>Original number : {{ number }}</h1>
     <h1>squared number : {{ squNum }}</h1>
+
+    <button @click="number++">++</button>
+    <button @click="decrement">--</button>
 </template>
