1db629b5b0b51a05a26f42b6a53e0b0e30103d92
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-07-05 22:22:27 +0600

    updated provide and inject component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script setup>
-import { provide } from 'vue';
-import ProvideInject from './Components3/ProvideInject.vue';
-provide('fName', 'sakil')
-provide('lName', 'mahmud')
-provide('friends', ['rasel', 'monir', 'jalil'])
-provide('movies', [
-  { name: '3 idiots', released: 2007, genre: 'educational', language: 'hindi' },
-  { name: 'tare jamin par', released: 2000, genre: 'educational', language: 'hindi' },
-  { name: 'gazni', released: 2011, genre: 'action', language: 'hindi' },
-  { name: 'television', released: 2011, genre: 'educational', language: 'bangla' },
-])
+
+// import ProvideComponent from './Components3/ProvideComponent.vue';
+
 </script>
 
 <template>
-  <ProvideInject />
+  <!-- <ProvideComponent /> -->
 </template>
---
+++ src/Components3/ProvideComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { provide } from 'vue';
+
+import InjectComponent from './InjectComponent.vue';
+
+
+provide('fName', 'sakil')
+provide('lName', 'mahmud')
+provide('friends', ['rasel', 'monir', 'jalil'])
+provide('movies', [
+    { name: '3 idiots', released: 2007, genre: 'educational', language: 'hindi' },
+    { name: 'tare jamin par', released: 2000, genre: 'educational', language: 'hindi' },
+    { name: 'gazni', released: 2011, genre: 'action', language: 'hindi' },
+    { name: 'television', released: 2011, genre: 'educational', language: 'bangla' },
+])
+
+</script>
+
+<template>
+    <InjectComponent />
+</template>
