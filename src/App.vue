9213fa7998f3661d4537e8d8a19189131b278b80
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-07-06 00:53:55 +0600

    reactive watch hook done

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
 // import ProvideComponent from './Components3/ProvideComponent.vue';
 // import LifecycleHooksContainerComponent from './Components3/LifecycleHooksContainerComponent.vue';
 
-import WatcherComponent from "./Components3/WatcherComponent.vue"
+// import WatcherComponent from "./Components3/WatcherComponent.vue"
+import ReactiveWatcherComponent from "./Components3/ReactiveWatcherComponent.vue"
 
 </script>
 
 <template>
-  <WatcherComponent />
+  <ReactiveWatcherComponent />
+  <!-- <WatcherComponent /> -->
   <!-- <LifecycleHooksContainerComponent /> -->
   <!-- <ProvideComponent /> -->
 </template>
---
+++ src/Components3/ReactiveWatcherComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { reactive, ref, watch } from "vue";
+const state = reactive({
+    search: ''
+})
+const details = ref('')
+
+watch(() => state.search, (newVal, oldVal) => {
+
+    console.log(newVal, oldVal);
+})
+
+</script>
+
+<template>
+    <h1>Reactive Watch</h1>
+    <hr>
+    <h2 v-if="details">{{ details }}</h2>
+    <input v-model="state.search" placeholder="write some text here..." type="text">
+
+
+
+</template>
