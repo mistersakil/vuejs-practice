b498f53376fc7dc345b748d25baa59be0a0d05ef
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-07-05 22:31:12 +0600

    lifecycle hooks onBeforeMound onMouted onBeforeUpdate onUpdated done

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 
 // import ProvideComponent from './Components3/ProvideComponent.vue';
+import LifecycleHooks from './Components3/LifecycleHooks.vue';
 
 </script>
 
 <template>
+  <LifecycleHooks />
   <!-- <ProvideComponent /> -->
 </template>
---
+++ src/Components3/LifecycleHooks.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated } from "vue";
+
+const count = ref(0)
+
+onBeforeMount(() => {
+    console.log('component about to mount');
+})
+
+onMounted(() => {
+    console.log('component has been mounted');
+})
+
+onBeforeUpdate(() => {
+    console.log('component about to update');
+})
+
+
+onUpdated(() => {
+    console.log('component has been updated');
+})
+
+</script>
+
+<template>
+    <h1>Lifecycle hooks</h1>
+    <hr>
+
+    <h2>Count: {{ count }}</h2>
+    <button @click="count++">++ Increment</button>
+    <button @click="count--">-- Decrement</button>
+    <button @click="count = 0">Reset</button>
+
+</template>
