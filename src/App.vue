e2c33a174764784690c0cd2240b6328ae14a71a1
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-07-05 19:57:23 +0600

    vue provide and inject learnt

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <script setup>
-
-import SlotComponent from './Components3/SlotComponent.vue';
-
+import { provide } from 'vue';
+import ProvideInject from './Components3/ProvideInject.vue';
+provide('fName', 'sakil')
+provide('lName', 'mahmud')
+provide('friends', ['rasel', 'monir', 'jalil'])
+provide('movies', [
+  { name: '3 idiots', released: 2007, genre: 'educational', language: 'hindi' },
+  { name: 'tare jamin par', released: 2000, genre: 'educational', language: 'hindi' },
+  { name: 'gazni', released: 2011, genre: 'action', language: 'hindi' },
+  { name: 'television', released: 2011, genre: 'educational', language: 'bangla' },
+])
 </script>
 
 <template>
-  <SlotComponent>
-    <h2>passing slot data from parent component</h2>
-
-    <template #one>
-      <h3>passing data from parent component to slot "one"</h3>
-    </template>
-
-    <template v-slot:two>
-      <h3>passing data from parent component to slot "two"</h3>
-    </template>
-
-  </SlotComponent>
-
+  <ProvideInject />
 </template>
---
+++ src/Components3/ProvideInject.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { inject } from "vue";
+
+const firstName = inject('fName');
+const lastName = inject('lName');
+const myFriends = inject('friends');
+const myMovies = inject('movies');
+
+</script>
+
+<template>
+    <h2>First name: {{ firstName }}</h2>
+
+    <h2>Last name: {{ lastName }}</h2>
+
+    <h2>My Friends</h2>
+
+    <ul v-for="(friend, index) in myFriends" :key="index">
+        <li>{{ friend }}</li>
+    </ul>
+    <h2>My Movies</h2>
+    <ul v-for="({ name, released, genre, language }, index) in myMovies" :key="index">
+        <li>Movie name: {{ name }}</li>
+        <li>Movie released: {{ released }}</li>
+        <li>Movie genre: {{ genre }}</li>
+        <li>Movie language: {{ language }}</li>
+    </ul>
+
+</template>
