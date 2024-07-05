444c6df781e8862a5306a67b40b3bf8e339afe06
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-07-05 19:11:24 +0600

    learnt slot

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,15 @@ import SlotComponent from './Components3/SlotComponent.vue';
 
 <template>
   <SlotComponent>
-    <h2>slot as children in react js</h2>
+    <h2>passing slot data from parent component</h2>
+
+    <template #one>
+      <h3>passing data from parent component to slot "one"</h3>
+    </template>
+
+    <template v-slot:two>
+      <h3>passing data from parent component to slot "two"</h3>
+    </template>
 
   </SlotComponent>
 
--- src/Components3/SlotComponent.vue
+++ src/Components3/SlotComponent.vue
@@ -5,6 +5,14 @@
 <template>
     <h1>Learning Slot</h1>
     <hr>
-    <slot></slot>
+    <slot>
+        <h3>default slot data will be shown when no data provided by parent component</h3>
+    </slot>
+
+    <slot name="one"></slot>
+
+    <slot name="two"></slot>
+
+
 
 </template>
