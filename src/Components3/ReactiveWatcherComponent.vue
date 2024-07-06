a460f2e1b0ef4d51778735fd12024fcd51c9d4a2
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-07-07 00:25:03 +0600

    reactive watcher with multiple sources

--- src/Components3/ReactiveWatcherComponent.vue
+++ src/Components3/ReactiveWatcherComponent.vue
@@ -3,15 +3,36 @@ import { reactive, ref, watch } from "vue";
 const state = reactive({
     search: ''
 })
-const details = ref('')
 
-watch(() => state.search, (newVal, oldVal) => {
-    if (newVal.length) {
-        details.value = `Your input: ${newVal}`
+const counter = reactive({
+    count: 0
+})
+
+const details = ref('')
+const countDetails = ref(0)
+
+watch(() => {
+    return { search: state.search, count: counter.count, something: 'what' }
+}, (newVal, oldVal) => {
+    console.log(newVal, oldVal)
+    if (newVal.search.length) {
+        details.value = `Search text: ${newVal.search}`
     } else {
         details.value = '';
     }
+    if (newVal.count) {
+        countDetails.value = `Counter value: ${newVal.count}`
+    } else {
+        countDetails.value = '';
+    }
 })
+
+const resetValues = () => {
+    state.search = '';
+    counter.count = 0;
+    details.value = '';
+    countDetails.value = '';
+}
 
 </script>
 
@@ -20,6 +41,14 @@ watch(() => state.search, (newVal, oldVal) => {
     <hr>
     <h2 v-if="details">{{ details }}</h2>
     <input v-model="state.search" placeholder="write some text here..." type="text">
+
+    <hr>
+    <h2 v-if="countDetails">{{ countDetails }}</h2>
+
+    <button @click="counter.count++">++ increment</button>
+    <button @click="counter.count--">-- decrement</button>
+
+    <button @click="resetValues">reset</button>
 
 
 
