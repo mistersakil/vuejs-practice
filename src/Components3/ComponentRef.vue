c849bcf342e46d1fda57645486bba19f6bac28c8
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-07-07 20:13:46 +0600

    watchEffect and watch with ref

--- src/Components3/ComponentRef.vue
+++ src/Components3/ComponentRef.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, watch } from 'vue'
+import { ref, onMounted, watch, watchEffect } from 'vue'
 const nameRef = ref('aaa')
 const nameDetails = ref('');
 // watch(nameRef, (newVal, oldVal) => {
@@ -7,16 +7,36 @@ const nameDetails = ref('');
 //     nameDetails.value = `Result: ${newVal}`
 // });
 
-onMounted(() => {
-    console.log(nameRef.value.value);
-    nameRef.value.focus();
-})
+// onMounted(() => {
+//     console.log(nameRef.value.value);
+//     nameRef.value.focus();
+// })
+
+const search = ref(null);
+
+// console.log(search.value);
+
+watch(search, (newVal, oldVal) => {
+    console.log(newVal, oldVal);
+    console.log('running watchEffect');
+    console.log(search.value);
+
+    if (search.value) {
+        console.log(search.value);
+    } else {
+        console.log('component did not mount yet...');
+    }
+});
 </script>
 
 <template>
-    <h1>Component Ref</h1>
+    <h1>component ref with watchEffect</h1>
+    <hr>
+    <input ref="search" type="search">
+
+    <!-- <h1>Simple Component Ref</h1>
     <hr>
     <input ref="nameRef" type="text" placeholder="write something">
 
-    <h3 v-if="nameDetails">{{ nameDetails }}</h3>
+    <h3 v-if="nameDetails">{{ nameDetails }}</h3> -->
 </template>
