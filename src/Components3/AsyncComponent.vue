0e3f868f8a2e9382db97153f6ead0c111d8081a2
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-07-08 09:37:16 +0600

    applied defineAsyncComponent

--- src/Components3/AsyncComponent.vue
+++ src/Components3/AsyncComponent.vue
@@ -1,7 +1,16 @@
 <script setup>
+import { ref, defineAsyncComponent } from "vue";
+
+const toggleUser = ref(false);
+
+const UsersComponents = defineAsyncComponent(() => {
+    import("./UsersComponent.vue")
+});
 </script>
 
 <template>
     <h1>Async Component</h1>
+    <button @click="toggleUser = !toggleUser">toggle users</button>
     <hr>
+    <UsersComponents v-if="toggleUser" />
 </template>
---
+++ src/Components3/UsersComponent.vue
@@ -0,0 +1,12 @@
+<script setup>
+const users = ['sakil', 'rasel', 'monir'];
+</script>
+
+<template>
+    <h1>User list</h1>
+    <hr>
+
+    <ul v-for="(user, index) in users" :key="index">
+        <li>{{ user }}</li>
+    </ul>
+</template>
