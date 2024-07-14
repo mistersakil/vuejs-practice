ae107bcb9c65227bbb45701ee7b233fa17087d2f
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-07-14 22:02:53 +0600

    use of image on vue component done

--- src/Components3/ImageInVue.vue
+++ src/Components3/ImageInVue.vue
@@ -1,10 +1,18 @@
 <script setup>
+import { ref } from "vue"
+
+const imgPath = ref("./src/assets/images/sakil2.jpg")
 
 </script>
 
 <template>
     <h1>How to use image in vue</h1>
     <hr>
+    <h3>relative path binding</h3>
+    <img src="../assets/images/sakil1.jpg" alt="sakil1" width="200">
+    <br>
+    <h3>absolute path binding</h3>
+    <img v-bind:src="imgPath" alt="sakil2" width="200">
 
 
 
