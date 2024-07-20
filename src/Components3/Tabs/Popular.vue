cd603ab4b830e4568249236702f11eee8c570142
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-07-20 14:14:32 +0600

    Tabs without dynamic component

---
+++ src/Components3/Tabs/Popular.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Popular post</h1>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate error est sed in officia non voluptatem enim
+        nisi iusto corporis vel esse qui architecto soluta, repellat excepturi itaque at exercitationem!
+    </p>
+
+
+</template>
--- src/Components3/TabsComponent.vue
+++ src/Components3/TabsComponent.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { ref } from 'vue'
 
+import Latest from "./Tabs/Latest.vue"
+import Popular from "./Tabs/Popular.vue"
+import Featured from "./Tabs/Featured.vue"
+
 const activeTab = ref('latest')
 
 </script>
@@ -12,5 +16,9 @@ const activeTab = ref('latest')
     <button @click="activeTab = 'latest'">latest</button>
     <button @click="activeTab = 'popular'">popular</button>
     <button @click="activeTab = 'featured'">featured</button>
+
+    <Latest v-if="activeTab === 'latest'" />
+    <Popular v-if="activeTab === 'popular'" />
+    <Featured v-if="activeTab === 'featured'" />
 
 </template>
