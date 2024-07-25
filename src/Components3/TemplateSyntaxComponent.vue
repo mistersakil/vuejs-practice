d8e32fcfd4a523ac35877e85ca91eaa9004bf081
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-07-25 23:46:30 +0600

    use of v-html directive

--- src/Components3/TemplateSyntaxComponent.vue
+++ src/Components3/TemplateSyntaxComponent.vue
@@ -2,6 +2,8 @@
 
 const txt = `some text`;
 
+const vHtml = `<span style="color:red">this is a red text</span>`
+
 </script>
 
 
@@ -13,5 +15,8 @@ const txt = `some text`;
     <del>text interpolation</del>
 
     <p>message: {{ txt }}</p>
+
+    <del>using v-html directive</del>
+    <h4 v-html="vHtml"></h4>
 
 </template>
