dfe3fdd8a3607434acb834581999e0adcf2c5a00
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-07-26 04:32:23 +0600

    v-model using defineModel() macro

---
+++ src/Components3/ComponentVModelChild.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { watch } from 'vue'
+const modelable = defineModel()
+const { inputLabel, inputId } = defineProps(['inputLabel', 'inputId'])
+
+// console.log(modelable.value);
+
+console.log(inputLabel, inputId);
+
+watch(modelable, (newVal, oldVal) => {
+    console.log(modelable.value);
+    console.log(newVal);
+    console.log(oldVal);
+})
+
+</script>
+
+<template>
+
+    <div>
+        <label v-bind:for="inputId">{{ inputLabel }}: </label>
+        <input v-bind:id="inputId" v-model="modelable">
+    </div>
+</template>
+
+
+<style>
+blockquote {
+    background-color: gray;
+    padding: 10px;
+}
+</style>
--- src/Components3/ComponentVmodel.vue
+++ src/Components3/ComponentVmodel.vue
@@ -1,9 +1,24 @@
-<script setup></script>
+<script setup>
+import ComponentVModelChild from "./ComponentVModelChild.vue"
+import { reactive } from "vue"
+
+const state = reactive({
+    name: '',
+    email: '',
+    password: '',
+})
+
+</script>
 
 <template>
-    <h1>Component v-model</h1>
+    <blockquote>v-model using defineModel() macro, need vue 3.4 minimum</blockquote>
     <hr>
+    <pre>{{ state }}</pre>
+    <blockquote>applying v-model on input</blockquote>
 
+    <ComponentVModelChild v-model="state.name" inputId="name" inputLabel="full name" /> <br>
+    <ComponentVModelChild v-model="state.email" inputId="email" inputLabel="email address" /> <br>
+    <ComponentVModelChild v-model="state.password" inputId="password" inputLabel="password" />
 </template>
 
 
